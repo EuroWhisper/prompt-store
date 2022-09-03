2ad727a18fbe877b4375871e82c62b4aac60d2a8
Ralph Juden <ralphjuden@gmail.com>
2022-09-03 19:51:51 +0200

    Set up eslint and prettier configs

--- pages/api/prompt/create.ts
+++ pages/api/prompt/create.ts
@@ -1,24 +1,24 @@
-import { PrismaClient, prompt } from "@prisma/client";
-import type { NextApiRequest, NextApiResponse } from "next";
-import validator from "validator";
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient, prompt } from '@prisma/client';
+import validator from 'validator';
 
 const prisma = new PrismaClient();
 
 export type Prompt = prompt;
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Prompt | null>
+    req: NextApiRequest,
+    res: NextApiResponse<Prompt | null>
 ) {
-  if (req.method === "POST") {
-    const { prompt } = req.body;
-    const sanitizedPrompt = validator.escape(prompt);
+    if (req.method === 'POST') {
+        const { prompt } = req.body;
+        const sanitizedPrompt = validator.escape(prompt);
 
-    const newPrompt = await prisma.prompt.create({
-      data: {
-        prompt: sanitizedPrompt,
-      },
-    });
-    res.json(newPrompt);
-  }
+        const newPrompt = await prisma.prompt.create({
+            data: {
+                prompt: sanitizedPrompt,
+            },
+        });
+        res.json(newPrompt);
+    }
 }
--- pages/api/prompts.ts
+++ pages/api/prompts.ts
@@ -1,14 +1,14 @@
-import { PrismaClient, prompt } from "@prisma/client";
-import type { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient, prompt } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
 export type Prompt = prompt;
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Prompt[] | null>
+    req: NextApiRequest,
+    res: NextApiResponse<Prompt[] | null>
 ) {
-  const prompts = await prisma.prompt.findMany();
-  res.json(prompts);
+    const prompts = await prisma.prompt.findMany();
+    res.json(prompts);
 }
