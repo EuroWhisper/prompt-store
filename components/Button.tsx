3d65fab905515b7b50dc546defbaf2eb35ab3c82
Ralph Juden <ralphjuden@gmail.com>
2022-09-03 15:57:26 +0200

    Add copy cell to datagrid

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,5 +1,8 @@
+import { ReactNode } from "react";
+
 type Props = {
   onClick?: () => void;
+  children?: ReactNode;
 };
 
 export default function Button(props: Props) {
@@ -8,7 +11,7 @@ export default function Button(props: Props) {
       onClick={props.onClick}
       className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
     >
-      Button
+      {props.children}
     </button>
   );
 }
--- components/PromptList.tsx
+++ components/PromptList.tsx
@@ -1,5 +1,6 @@
 import { Prompt } from "../pages/api/prompt";
 import DataGrid, { Column } from "react-data-grid";
+import Button from "./Button";
 
 type Props = {
   prompts: Prompt[];
@@ -9,9 +10,24 @@ export default function PromptList(props: Props) {
   const columns: Column<Prompt>[] = [
     { key: "id", name: "ID", width: 50 },
     { key: "prompt", name: "Prompt", minWidth: 500 },
+    {
+      key: "copy",
+      name: "Copy",
+      width: 50,
+      formatter: (column) => (
+        <Button onClick={() => handleCopyPrompt(column.row.prompt)}>
+          Copy
+        </Button>
+      ),
+    },
   ];
 
   const rows = props.prompts;
 
   return <DataGrid columns={columns} rows={rows} />;
+
+  function handleCopyPrompt(prompt: string) {
+    navigator.clipboard.writeText(prompt);
+    alert("Copied to clipboard!");
+  }
 }
